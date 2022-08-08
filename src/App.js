5fe62704fcd27ef80800a56b7214f2cbf401ed69
skyhigh10 <ezekielalokwu99@gmail.com>
2022-08-08 16:46:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Nav from "./Components/Nav/Nav"
+import Banner from "./Components/Banner/Banner"
+import Movies from "./Components/Movies/Movies"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+class App extends React.Component{
+    constructor(){
+      super()
+    }
+    render(){
+      return(
+        <React.Fragment>
+          <Nav/>
+          <div>
+           <Banner/>
+           <Movies/>
+          </div>
+        </React.Fragment>
+      )
+    }
+}
+
+export default App;
